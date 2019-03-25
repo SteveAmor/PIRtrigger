9f8bfa5c497a1f97a9c20aea95754fcbb41c370d
SteveAmor <SteveAmor@users.noreply.github.com>
2019-03-25 18:12:55 +0000

    Initial commit

---
+++ creds.py
@@ -0,0 +1,3 @@
+TO_EMAIL = 'foo@bar.com'
+FROM_EMAIL = 'bar@foo.com'
+EMAIL_PASS = 'foobar'
---
+++ trigger.py
@@ -0,0 +1,53 @@
+from creds import TO_EMAIL, FROM_EMAIL, EMAIL_PASS
+from gpiozero import Button
+from time import sleep
+import datetime
+import smtplib
+from email.mime.text import MIMEText
+
+lastTriggerDate = "01/01/1970"
+
+pir = Button(21)  # Physical pin 40 (ground it to trigger)
+
+def sendEmail():
+	# Define SMTP email server details
+	smtp_server = 'smtp.gmail.com:587'
+	smtp_user = FROM_EMAIL
+	smtp_pass = EMAIL_PASS
+	addr_from = smtp_user
+	addr_to = TO_EMAIL
+	subject = "First PIR trigger of the day"
+
+	text = datetime.datetime.now().strftime('%H:%M:%S GMT %d/%m/%Y')
+	msg = MIMEText(text)
+
+	msg['To'] = addr_to
+	msg['From'] = addr_from
+	msg['Subject'] = subject
+
+	try:
+		# Send the message via an SMTP server
+		s = smtplib.SMTP(smtp_server)
+		s.starttls()
+		s.login(smtp_user,smtp_pass)
+		s.sendmail(addr_from, addr_to, msg.as_string())
+		s.quit()
+		print("%s - Email Sent OK" %subject)
+
+	except smtplib.SMTPException:
+		print("%s - Email Send Fail" %subject)
+
+while True:
+	print("Ready...")
+	pir.wait_for_press()
+	print("PIR triggered")
+	triggerDate = datetime.datetime.now().strftime('%d/%m/%Y')
+	if triggerDate != lastTriggerDate:
+		sendEmail()
+		lastTriggerDate = triggerDate
+		print("Waiting 60 seconds.")
+	else:
+		print("Not the first trigger of the day.  No email sent.  Waiting 60 seconds.")
+
+	sleep(60)
+
